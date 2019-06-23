d0e948926d4cdc292d067c46a8730a81c117a6df
Aniket <39321546+an-k45@users.noreply.github.com>
2019-06-23 09:48:45 -0400

    Update parliament results to exclude Telangana

--- data/writers/write_parliament_results.py
+++ data/writers/write_parliament_results.py
@@ -24,16 +24,18 @@ def get_result_row(result: List[str], parliament_ids: Dict[str, int]) -> List[Un
 
 def main():
     # incoming results: 0:YEAR,1:STATE,2:PC,3:NAME,4:SEX,5:PARTY,6:AGE,7:CATEGORY,8:VOTES,9:ELECTORS,10:#
-    # incoming parliament seats: 0:ID,1:NAME,2:STATE_ID,3:CODE,4:CATEGORY,5:DELIMITATION
     with open("../srcdata/parliament/parliament_elections_data.csv", "r") as incoming_results_csv:
         incoming_results = list(csv.reader(incoming_results_csv))
     incoming_results_csv.close()
 
+    # incoming parliament seats: 0:ID,1:NAME,2:STATE_ID,3:CODE,4:CATEGORY,5:DELIMITATION
     with open("../datasets/parliament.csv", "r") as parliament_csv:
         parliament_seats = list(csv.reader(parliament_csv))
     parliament_csv.close()
 
-    parliament_ids = {parliament_seats[i][1]:parliament_seats[i][0] for i in range(1, len(parliament_seats))}
+    # key: name, value: id
+    # TODO: Change this to be more versatile, able to handle multiple years.
+    parliament_ids = {parliament_seats[i][1]: parliament_seats[i][0] for i in range(1, len(parliament_seats))}
 
     # NOTE: The relationship between pre-2008 seats to their result is lost here, and needs to be reestablished
     # by using source files above.
